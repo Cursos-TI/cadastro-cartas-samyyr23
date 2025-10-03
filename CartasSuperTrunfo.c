9b360a01fbfb55aa579494faa184ff4ba523a387
Samyr <155871484+samyyr23@users.noreply.github.com>
2025-10-03 15:39:56 +0000

    Criei todo código do Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,52 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+int main(){
+    char estado;
+    char codigo[3];
+    char cidade[50];
+    float populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+        printf("Seja Bem-Vindo ao Super Trunfo! Por favor, cadastre duas cartas:\n");
+        printf("Por favor, digite uma letra que represente seu estado escolhido (exemplo: Rio de Janeiro = R): ");
+        scanf("%c", &estado);
+        printf("Agora, digite um código de 01 a 04 para definir o número da carta: ");
+        scanf("%2s", codigo);
+        printf("Digite o nome da cidade escolhida: ");
+        scanf("%s", cidade);
+        printf("Qual o número de habitantes da cidade de %s? ", cidade);
+        scanf("%f", &populacao);
+        printf("Quantos quilômetros quadrados tem a área da cidade? ");
+        scanf("%f", &area);
+        printf("Qual o PIB da cidade? ");
+        scanf("%f", &pib);
+        printf("Quantos Pontos Turísticos há na cidade? ");
+        scanf("%d", &pontos_turisticos);
 
-  // Área para entrada de dados
+        printf("Esses são os dados de sua primeira carta:\n");
+        printf("Estado: %c\nCodigo: %c%s\nCidade: %s\nPopulação: %.2f\nÁrea: %.2f km²\nPIB: %.2f\nPontos Turísticos: %d\n", estado, estado, codigo, cidade, populacao, area, pib, pontos_turisticos);
 
-  // Área para exibição dos dados da cidade
+            printf("Por favor, cadastre sua segunda carta:\n");
+            printf("Digite novamente uma letra que represente seu estado escolhido (exemplo: Rio de Janeiro = R): ");
+            scanf(" %c", &estado);
+            printf("Agora, digite um código de 01 a 04 para definir o número da carta: ");
+            scanf("%2s", codigo);
+            printf("Digite o nome da cidade escolhida: ");
+            scanf("%s", cidade);
+            printf("Qual o número de habitantes da cidade de %s? ", cidade);
+            scanf("%f", &populacao);
+            printf("Quantos quilômetros quadrados tem a área da cidade? ");
+            scanf("%f", &area);
+            printf("Qual o PIB da cidade? ");
+            scanf("%f", &pib);
+            printf("Quantos Pontos Turísticos há na cidade? ");
+            scanf("%d", &pontos_turisticos);
+
+             printf("Esses são os dados de sua segunda carta:\n");
+            printf("Estado: %c\nCodigo: %c%s\nCidade: %s\nPopulação: %.2f\nÁrea: %.2f km²\nPIB: %.2f\nPontos Turísticos: %d\n", estado, estado, codigo, cidade, populacao, area, pib, pontos_turisticos);
+
 
 return 0;
-} 
+}
