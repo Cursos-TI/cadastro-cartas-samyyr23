30ebddd11a3ab65a1380b07aa38b7f52677bef4e
Samyr <155871484+samyyr23@users.noreply.github.com>
2025-10-27 14:17:32 +0000

    ATT

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,6 +8,7 @@ int main(){
     float area;
     float pib;
     int pontos_turisticos;
+    float densidade, percapita;
 
         printf("Seja Bem-Vindo ao Super Trunfo! Por favor, cadastre duas cartas:\n");
         printf("Por favor, digite uma letra que represente seu estado escolhido (exemplo: Rio de Janeiro = R): ");
@@ -16,17 +17,20 @@ int main(){
         scanf("%2s", codigo);
         printf("Digite o nome da cidade escolhida: ");
         scanf("%s", cidade);
-        printf("Qual o número de habitantes da cidade de %s? ", cidade);
+        printf("Qual o número de habitantes da cidade de %s? (Por favor, digite sem ponto ou vírgula.) ", cidade);
         scanf("%f", &populacao);
-        printf("Quantos quilômetros quadrados tem a área da cidade? ");
+        printf("Quantos quilômetros quadrados tem a área da cidade? (Por favor, digite sem ponto ou vírgula.) ");
         scanf("%f", &area);
-        printf("Qual o PIB da cidade? ");
+        printf("Qual o PIB da cidade? (Por favor, digite sem ponto ou vírgula.) ");
         scanf("%f", &pib);
         printf("Quantos Pontos Turísticos há na cidade? ");
         scanf("%d", &pontos_turisticos);
 
+        densidade = populacao / area;
+        percapita = pib / populacao;
+    
         printf("Esses são os dados de sua primeira carta:\n");
-        printf("Estado: %c\nCodigo: %c%s\nCidade: %s\nPopulação: %.2f\nÁrea: %.2f km²\nPIB: %.2f\nPontos Turísticos: %d\n", estado, estado, codigo, cidade, populacao, area, pib, pontos_turisticos);
+        printf("Estado: %c\nCodigo: %c%s\nCidade: %s\nPopulação: %.0f\nÁrea: %.0f km²\nPIB: %.0f\nPontos Turísticos: %d\nDensidade Populacional: %.2f\nPIB per Capita: %.2f\n", estado, estado, codigo, cidade, populacao, area, pib, pontos_turisticos, densidade, percapita);
 
             printf("Por favor, cadastre sua segunda carta:\n");
             printf("Digite novamente uma letra que represente seu estado escolhido (exemplo: Rio de Janeiro = R): ");
@@ -35,18 +39,21 @@ int main(){
             scanf("%2s", codigo);
             printf("Digite o nome da cidade escolhida: ");
             scanf("%s", cidade);
-            printf("Qual o número de habitantes da cidade de %s? ", cidade);
+            printf("Qual o número de habitantes da cidade de %s? (Por favor, digite sem ponto ou vírgula.) ", cidade);
             scanf("%f", &populacao);
-            printf("Quantos quilômetros quadrados tem a área da cidade? ");
+            printf("Quantos quilômetros quadrados tem a área da cidade? (Por favor, digite sem ponto ou vírgula.) ");
             scanf("%f", &area);
-            printf("Qual o PIB da cidade? ");
+            printf("Qual o PIB da cidade? (Por favor, digite sem ponto ou vírgula.) ");
             scanf("%f", &pib);
             printf("Quantos Pontos Turísticos há na cidade? ");
             scanf("%d", &pontos_turisticos);
 
-             printf("Esses são os dados de sua segunda carta:\n");
-            printf("Estado: %c\nCodigo: %c%s\nCidade: %s\nPopulação: %.2f\nÁrea: %.2f km²\nPIB: %.2f\nPontos Turísticos: %d\n", estado, estado, codigo, cidade, populacao, area, pib, pontos_turisticos);
+            densidade = populacao / area;
+            percapita = pib / populacao;
+
+            printf("Esses são os dados de sua segunda carta:\n");
+            printf("Estado: %c\nCodigo: %c%s\nCidade: %s\nPopulação: %.0f\nÁrea: %.0f km²\nPIB: %.0f\nPontos Turísticos: %d\nDensidade Populacional: %.2f\nPIB per Capita: %.2f\n", estado, estado, codigo, cidade, populacao, area, pib, pontos_turisticos, densidade, percapita);
 
 
-return 0;
+
 }
